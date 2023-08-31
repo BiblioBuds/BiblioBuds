d65cae2623e2d105b697e924b42aadc6cb3817c2
Juan David Perez <juandaperezvelez@hotmail.com>
2023-08-31 17:42:56 -0500

    Home Page Fixed

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,34 +1,99 @@
 "use client";
-import styles from './home.module.css';
-import PromoCarousel from '@/components/Carrusel/Carrusel';
+import axios from 'axios';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { useEffect, useState } from 'react';
+import { StarIcon } from "@heroicons/react/24/solid";
+import { useGlobalContext } from "@/app/Context/store";
 import Card from '@/components/cards/card';
 
 
-
 export const Home = () => {
-  
-   
 
+    // const { books } = useGlobalContext();
+    // console.log(books)
+  
+    const [books, setBooks] = useState();
+    const[books2, setBooks2] = useState()
+
+    const fetchBooks = async () => {
+        const data = await axios.get(`/api/books`)
+        .then((res) => res.data);
+        setBooks(data.slice(6,12));
+        setBooks2(data.slice(0,6))
+        console.log(data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    console.log(books)
+    console.log(books2)
 
   return (
-    <div className={styles.main}>
-        <header>
-          
-          <h1 className={styles.hometitle}>For You</h1>
-        </header>
-        <PromoCarousel />
-        <section className={styles.popular}>
-          <h3 className={styles.popularpad}>Popular</h3>
-          {/* <Card/> */}
-        </section>
-        {/* <section className={styles.cards}>
-          <Card/>
-         
-
-          <div className={styles.cardContainer}>
-            <Card></Card>
-          </div>
-        </section> */}
+    <div className='w-screen h-full justify-center'>
+        <div className='mb-2 h-80'>
+            <h1 className='text-3xl font-bold pt-4 ml-20'>For you</h1>
+            <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={125}
+            totalSlides={6}
+            visibleSlides={3}
+            step={1}
+            orientation="horizontal"
+            infinite={true} interval={4000} isPlaying={true}
+            >
+                <div className='overflow-x-hidden'>
+                <Slider className="mt-4 ml-4 justify-start transition ease-out duration-5000" classNameAnimation='transition duration-5000 ease-in-out'>
+                <div className="h-full flex lg:gap-12 md:gap-6 gap-12 justify-start transition ease-out duration-5000">
+                    {books?.length > 0 ?
+                    books?.map((book,id)=> {
+                        return (
+                            <Slide index={id}>
+                            <div className="grid grid-cols-2 bg-gradient-to-b from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% w-[400px] h-64 rounded-lg">
+                                    <div>
+                                        <img src={book?.image} alt="black chair and white table" className="w-32 h-56 mt-4 ml-4 rounded-lg grid-cols-1" />
+                                    </div>
+                                    <div className="justify-start mr-2">
+                                        <h2 className="mt-6 text-xl lg:text-xl font-semibold leading-5 lg:leading-6 text-white">{book?.title}</h2>
+                                        <h2 className="mt-1 text-lg lg:text-lg font-light italic leading-5 lg:leading-6 text-white">{book?.author}</h2>
+                                        <div className="flex flex-row mt-1">
+                                            <StarIcon className="h-6 w-6 text-white" />
+                                            <StarIcon className="h-6 w-6 text-white" />
+                                            <StarIcon className="h-6 w-6 text-white" />
+                                            <StarIcon className="h-6 w-6 text-white" />
+                                            <StarIcon className="h-6 w-6 text-white" />
+                                        </div>
+                                    </div>
+                                </div> 
+                            </Slide>
+                        )
+                    })
+                    : <p>Loading...</p>
+                    }
+                    </div>
+                </Slider>
+                </div>  
+            </CarouselProvider>
+        </div>
+        <div className="p-4 w-full ml-2">
+            <div className=" space-y-5">
+                <h1 className='text-3xl font-bold pt-4 ml-20'>Top Rated</h1>
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center items-center w-full mx-auto">
+                {books2?.length > 0 ? (
+                    books2?.map(
+                    (book) =>
+                        book.stock > 0 &&
+                        book.isActive && <Card key={book.id} book={book} />
+                    )
+                ) : (
+                    <p className="font-raleway text-lg font-bold text-center">
+                    Loading...
+                    </p>
+                )}
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
