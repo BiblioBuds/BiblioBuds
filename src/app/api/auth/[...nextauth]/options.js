4af2ddc43f714df105b6069a6e20b8deb7edc5ff
julivito22 <julietagimenez_6@hotmail.com>
2023-09-06 19:43:08 -0300

    cambios en account

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -20,6 +20,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      redirectUri: "http://localhost:3000/api/auth/callback/google"
     }),
     CredentialsProvider({
       name: "Credentials",
